90ac0b1bb204ea2f2f11835c6dff88f2afed751b
millerd <millerdicarlo@gmail.com>
2022-05-23 14:58:37 -0400

    functioning code

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,31 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
     event.preventDefault();
 
+    //entered value for task name and type will now print
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-  };
+
+    console.dir(listItemEl);
+  }; // end of createTaskHandler function
 
 
 
